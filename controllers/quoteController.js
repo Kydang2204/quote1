fcfdcf1eb4ad2c58c5a44981c0f7ad9c0216b810
andychicken <83952965+andychicken@users.noreply.github.com>
2021-05-19 11:25:59 +0700

    last

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -1,3 +1,4 @@
+const { Long } = require('bson');
 const express = require('express');
 const quote=require('../models/quotes');
 const app = express();
@@ -8,15 +9,12 @@ app.get('/',(req,res)=>
 })
 app.post('/add',async(req,res)=>
 {
-    const q=new quote(req.body);
-    try{
-        await q.save();
-        res.render('user/quote.hbs',{title:"Input quote successful"})
-
-    }catch{ (error)=>{
-        res.status(500).send(error);
-    }
-    }
+  if(req.body.id==''){
+      addrecord(req,res);
+  }
+  else{
+      updaterecord(req,res);
+  }
 
 })
 
@@ -38,8 +36,43 @@ app.get('/edit/:id',(req,res)=>{
 
     })
 })
+
+function addrecord(req,res){
+    const q=new quote(req.body);
+    try{
+         q.save();
+        res.render('user/quote.hbs',{title:"Input quote successful"})
+
+    }catch{ (error)=>{
+        res.status(500).send(error);
+    }
+    }
+}
     
-
-
+function updaterecord(req,res)
+{
+    quote.findOneAndUpdate({_id:req.body.id},req.body,{new:true},(err,doc)=>
+    {
+        if(!err){
+            res.redirect('/quote/list');
+        }else{
+            console.log(err);
+            res.render('user/add.hbs',{
+                title:'Error update'
+            })
+        }
+    })
+}
+//delete
+app.get('/delete/:id',async(req,res)=>{
+    try{
+        const qd= await quote.findByIdAndDelete(req.params.id,req.body);
+        if(!qd) res.status(404).send('No item found');
+        else res.redirect('/quote/list')
+    }catch(error)
+    {
+        res.status(500).send(error);
+    }
+})
 
 module.exports=app;
