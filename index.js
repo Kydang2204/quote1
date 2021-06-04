0ddd8ce331675d4bb81ac8f84e3b88cb12e8b246
andychicken <83952965+andychicken@users.noreply.github.com>
2021-06-04 12:25:10 +0700

    Keep calm and commit

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(express.json());
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 
+
+
+
+
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 app.engine('.handlebars', hdbs());
 app.set('view engine', '.handlebars');
