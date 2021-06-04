9ad53d5c8bc1dd2e89c840dbff7e5ba2b8f9af38
andychicken <83952965+andychicken@users.noreply.github.com>
2021-06-04 12:13:18 +0700

    Keep calm and commit

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const mongoose = require('mongoose');
 const hdbs = require('express-handlebars');
 const quotecontroller = require('./controllers/quoteController');
 
-
-
-
-
-
-
 const url = `mongodb+srv://andy:${process.env.MK}@cluster0.ixw6l.mongodb.net/abc?retryWrites=true&w=majority`;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
